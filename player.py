3f4e2279664722a5dcda58216b218903cb052881
Halmai Mirella <halmaimirella@gmail.com>
2019-01-14 11:57:06 +0100

    putting necessary data to player

--- player.py
+++ player.py
@@ -9,3 +9,39 @@ class Player:
     def showdown(self, game_state):
         pass
 
+    def do_raise(self, game_state):
+        pass
+
+    def get_current_buy_in(self, game_state):
+        if game_state:
+            return game_state['current_buy_in']
+
+    def get_minimum_raise(self, game_state):
+        if game_state:
+            return game_state['minimum_raise']
+
+    def get_our_player(self, game_state):
+        our_player_index = None
+        our_player = None
+
+
+     def sthg(self):
+        our_cards = None
+        community_cards = None
+        try:
+            our_player_index = game_state['in_action']
+        except KeyError as e:
+            pass
+        if our_player_index:
+            our_player = game_state['players'][our_player_index]
+            if our_player:
+                sys.stderr.write("\n\n\n{}\n\n".format(our_player))
+        if our_player:
+            try:
+                our_cards = our_player['hole_cards']
+            except KeyError as e:
+                pass
+        try:
+            community_cards = game_state['community_cards']
+        except KeyError as e:
+            pass
