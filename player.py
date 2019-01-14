ecba5ba7abd90f778553503fd7761ee1580b4791
gaborkallos <gaborkallos@gmail.com>
2019-01-14 10:50:29 +0100

    Version_0.2 Raise with minimum

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
 
     def betRequest(self, game_state):
         print(game_state)
-        return game_state["current_buy_in"] + game_state["minimum_raise"]
+        return int(game_state["current_buy_in"]) + int(game_state["minimum_raise"])
 
     def showdown(self, game_state):
         pass
