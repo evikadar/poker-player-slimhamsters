b2fd90b67882abc49c330626b50852eb25a783bd
Halmai Mirella <halmaimirella@gmail.com>
2019-01-14 11:31:18 +0100

    6th test for write to standard error

--- player_service.py
+++ player_service.py
@@ -39,10 +39,14 @@ class PlayerService(BaseHTTPServer.BaseHTTPRequestHandler):
 
         if game_state:
             current_buy_in = game_state['current_buy_in']
-            our_player_index = game_state['in_action']
-            our_player = game_state['players'][our_player_index]
-            if our_player:
-                sys.stderr.write("\n\n\n%s\n\n", our_player)
+            try:
+                our_player_index = game_state['in_action']
+            except KeyError as e:
+                pass
+            if our_player_index:
+                our_player = game_state['players'][our_player_index]
+                if our_player:
+                    sys.stderr.write("\n\n\n{}\n\n".format(our_player))
 
 
 
