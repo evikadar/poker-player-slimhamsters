db8f25a910362f6178c7bcb9da979d19ff4679c3
Halmai Mirella <halmaimirella@gmail.com>
2019-01-14 13:46:03 +0100

    fix self arguments 2

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ class Player:
     VERSION = "Version_0.5"
 
     def betRequest(self, game_state):
-        raise_value = self.check_our_hand(self, game_state)
+        raise_value = self.check_our_hand(game_state)
         return raise_value
 
     def showdown(self, game_state):
@@ -21,7 +21,6 @@ class Player:
                         return self.get_current_buy_in(game_state) + self.get_minimum_raise(game_state)
             elif our_cards[0]['rank'] in "JQKA" and our_cards[1]['rank'] in "JQKA":
                 return self.get_current_buy_in(game_state) + self.get_minimum_raise(game_state)
-
 
 
     def do_raise(self, game_state):
