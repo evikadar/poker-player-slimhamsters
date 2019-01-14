50675b2bf84ae6fa62edd9a6ed1266fc9d84bd5c
gaborkallos <gaborkallos@gmail.com>
2019-01-14 10:49:01 +0100

    Version_0.2 Raise with minimum

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 
 class Player:
-    VERSION = "Version_0.1"
+    VERSION = "Version_0.2"
 
     def betRequest(self, game_state):
         print(game_state)
-        return 1
+        return game_state["current_buy_in"] + game_state["minimum_raise"]
 
     def showdown(self, game_state):
         pass
